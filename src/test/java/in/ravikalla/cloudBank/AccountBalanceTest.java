6b2a054d9715d410faad5a5a08694fba31f574c6
ravi_soda <raviprasad38@gmail.com>
2017-12-02 22:35:02 +0530

    Addding Spring security testing at BDD level

--- src/test/java/in/ravikalla/cloudBank/AccountBalanceTest.java
+++ src/test/java/in/ravikalla/cloudBank/AccountBalanceTest.java
@@ -1,3 +1,48 @@
+<<<<<<< HEAD
+package in.ravikalla.cloudBank;
+
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Configuration;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.*;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.*;
+
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest
+@ContextConfiguration
+@WebAppConfiguration
+public class AccountBalanceTest {
+ 
+  @Autowired
+  WebApplicationContext context;
+  
+  MockMvc mockMvc;
+  
+  @Before
+  public void setUp(){
+    mockMvc = MockMvcBuilders.webAppContextSetup(context).apply(springSecurity()).build();
+  }
+  
+  @Test
+  public void test() throws Exception{
+    mockMvc.perform(get("/account/primaryAccount").with(user("admin").password("password"))).andExpect(status().isOk());
+  }
+}
+=======
 package in.ravikalla.cloudBank;
 
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
@@ -41,3 +86,4 @@ public class AccountBalanceTest {
     mockMvc.perform(get("/account/primaryAccount").with(user("user"))).andExpect(status().isOk());
   }
 }
+>>>>>>> 15262ff7dafd987fba5b39c0dab751a48ab757e3
