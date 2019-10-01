c254826fba19ef60bc353d0b977ba0607bc56d1b
Ravi Kalla <ravi2523096@gmail.com>
2019-10-01 01:18:22 -0400

    Fix for transaction update flow

--- src/main/java/in/ravikalla/cloudBank/domain/PrimaryTransaction.java
+++ src/main/java/in/ravikalla/cloudBank/domain/PrimaryTransaction.java
@@ -14,7 +14,7 @@ import javax.persistence.ManyToOne;
 public class PrimaryTransaction {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private Date date;
     private String description;
--- src/main/java/in/ravikalla/cloudBank/domain/SavingsAccount.java
+++ src/main/java/in/ravikalla/cloudBank/domain/SavingsAccount.java
@@ -17,7 +17,7 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 public class SavingsAccount {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private int accountNumber;
     private BigDecimal accountBalance;
